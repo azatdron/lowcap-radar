411070b0cb3882e4d30a8187a4864e29b6b004b8
azatdron <azatrafikov85@gmail.com>
2026-05-11 16:12:37 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-self.addEventListener('fetch',()=>{});
+const CACHE='lowcap-radar-v3-2';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>clients.claim());
+self.addEventListener('fetch',e=>{});

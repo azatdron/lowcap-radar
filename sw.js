15bc62540a7fb2557b487e8f96e0de03a00bec06
azatdron <azatrafikov85@gmail.com>
2026-05-11 14:07:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lowcap-radar-v13';
+const CACHE='lowcap-radar-v14';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.json','icon-192.png','icon-512.png'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.url.includes('api.coingecko.com'))return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});

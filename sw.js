63fedb5db514921bb7026b5c8b47580474e8868b
azatdron <azatrafikov85@gmail.com>
2026-05-11 13:58:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE='lowcap-radar-v1-2';
-const ASSETS=['./','index.html','manifest.json','sw.js','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
+const CACHE='lowcap-radar-v13';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.json','icon-192.png','icon-512.png'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{const req=e.request;if(req.method!=='GET')return;e.respondWith(fetch(req).then(res=>{if(new URL(req.url).origin===location.origin){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));}return res}).catch(()=>caches.match(req).then(r=>r||caches.match('index.html'))))});
+self.addEventListener('fetch',e=>{if(e.request.url.includes('api.coingecko.com'))return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});

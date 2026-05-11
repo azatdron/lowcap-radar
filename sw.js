41a1ef849438136517b56a38a4ef7595583834b2
azatdron <azatrafikov85@gmail.com>
2026-05-11 15:19:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,5 @@
-const CACHE='lowcap-radar-v2-0-1';
+const CACHE='lowcap-radar-v2-1';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.hostname.includes('api.dexscreener.com')) return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))));
-});
+self.addEventListener('fetch',e=>{ if(new URL(e.request.url).origin!==location.origin) return; e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});

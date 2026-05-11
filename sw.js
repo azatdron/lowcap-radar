922bf2c19f5765e0ed6f666713d13c38b332a0ef
azatdron <azatrafikov85@gmail.com>
2026-05-11 08:01:44 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='lowcap-radar-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
